ee59c55d2b4a9a399adb7df3fdd7d415ca09b7f6
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2024-05-03 12:17:44 -0500

    more basic config files added

---
+++ undrgrnd-tipping-app/server/controllers/warpcastController.js
@@ -0,0 +1,57 @@
+const warpcastService = require('../services/warpcastService');
+
+// Get user profile
+exports.getUserProfile = async (req, res, next) => {
+  try {
+    const { fid } = req.params;
+
+    // Fetch user profile from Warpcast API
+    const userProfile = await warpcastService.getUserProfile(fid);
+
+    res.status(200).json(userProfile);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get channel details
+exports.getChannelDetails = async (req, res, next) => {
+  try {
+    const { channelId } = req.params;
+
+    // Fetch channel details from Warpcast API
+    const channelDetails = await warpcastService.getChannelDetails(channelId);
+
+    res.status(200).json(channelDetails);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get user's following channels
+exports.getUserFollowingChannels = async (req, res, next) => {
+  try {
+    const { fid } = req.params;
+
+    // Fetch user's following channels from Warpcast API
+    const followingChannels = await warpcastService.getUserFollowingChannels(fid);
+
+    res.status(200).json(followingChannels);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Check if user is following a channel
+exports.isUserFollowingChannel = async (req, res, next) => {
+  try {
+    const { fid, channelId } = req.params;
+
+    // Check if user is following the channel using Warpcast API
+    const isFollowing = await warpcastService.isUserFollowingChannel(fid, channelId);
+
+    res.status(200).json({ isFollowing });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ undrgrnd-tipping-app/server/routes/warpcast.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const warpcastController = require('../controllers/warpcastController');
+
+// Get user profile
+router.get('/user/:fid', warpcastController.getUserProfile);
+
+// Get channel details
+router.get('/channel/:channelId', warpcastController.getChannelDetails);
+
+// Get user's following channels
+router.get('/user/:fid/following', warpcastController.getUserFollowingChannels);
+
+// Check if user is following a channel
+router.get('/user/:fid/following/:channelId', warpcastController.isUserFollowingChannel);
+
+module.exports = router;
